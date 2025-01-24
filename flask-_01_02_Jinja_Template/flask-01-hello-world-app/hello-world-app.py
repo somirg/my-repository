4a443acd36bdf3d4bac3cf492b82fb59b5c23bb6
somirg <somi.rg@gmail.com>
2025-01-23 21:38:29 -0500

    finished flask1 update

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,26 @@
-from flask import Flask 
+from flask import Flask
+
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello World"
 
+@app.route("/bye")
+def goodbye():
+    return "Goodbye"
 
+@app.route("/third/subthird")
+def third():
+    return "This is a double path"
 
+@app.route("/fourth/<string:id>")
+def fourth(id):
+    output = f"Your ID is {id}"
+    return output
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=5000)
     # app.run(host= '0.0.0.0', port=8081)
--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,13 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return render_template("index.html", number1=10, number2=20)
 
-
-
-
-
-
+@app.route("/<string:num1>/<string:num2>")
+def numbers(num1, num2):
+    return render_template("index.html", number1=num1, number2=num2)
+@app.route("/sum/<string:first>/<string:second>")
+def sum(first, second):
+    if first.isdigit() and second.isdigit():
+        num1 = int(first)
+        num2 = int(second)
+        numsum = num1 + num2
+        return render_template("body.html", value1=num1, value2=num2, sum=numsum)
+    else:
+        return "Poorly formatted URL"
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
+    app.run(debug=True, port=5000)
     # app.run(host= '0.0.0.0', port=8081)
